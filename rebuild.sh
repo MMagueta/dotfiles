f24c1f67fc961065c467736ea93e42fb352a67d7
Marcos Magueta <maguetamarcos@gmail.com>
2022-02-02 20:28:05 -0300

    fix: add fallback for python building

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -eux
-sudo nix build .#darwinConfigurations.MacMini.system
-sudo ./result/sw/bin/darwin-rebuild switch --flake .
+sudo nix build .#darwinConfigurations.MacMini.system --fallback
+sudo ./result/sw/bin/darwin-rebuild switch --flake . --fallback
